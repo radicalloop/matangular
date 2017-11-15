40495e1f13a18c9631ce2181d3d873cefe666726
Swati <swati.radicalloop@gmail.com>
2017-11-15 20:05:40 +0530

    left sidebar  changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -14,6 +14,14 @@ import {
   MatSlideToggleModule,
 } from '@angular/material';
 
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
+
 @NgModule({
   imports: [
     MatButtonModule,
@@ -26,7 +34,8 @@ import {
   	MatCheckboxModule,
   	MatListModule,
   	MatSlideToggleModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    PerfectScrollbarModule
   ],
   exports: [
     MatButtonModule,
@@ -39,8 +48,15 @@ import {
     MatCheckboxModule,
     MatListModule,
     MatSlideToggleModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    PerfectScrollbarModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
+  ]
 })
 export class SharedModule { }
