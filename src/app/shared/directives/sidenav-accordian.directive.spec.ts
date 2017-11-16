d5cc9a14c85bb86d2a2526f151b77504f62bdd07
Swati <swati.radicalloop@gmail.com>
2017-11-16 18:41:50 +0530

    sidebar changes

--- src/app/shared/directives/sidenav-accordian.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SidenavAccordianDirective } from './sidenav-accordian.directive';
-
-describe('SidenavAccordianDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SidenavAccordianDirective();
-    expect(directive).toBeTruthy();
-  });
-});
