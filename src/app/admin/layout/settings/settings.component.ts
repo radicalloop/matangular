e037c2d078a289a9b0ddcab7c4c025be4ed2cfd2
jimit shah <jimit.it@gmail.com>
2017-12-08 16:06:18 +0530

    fix some changes

--- src/app/admin/layout/settings/settings.component.ts
+++ src/app/admin/layout/settings/settings.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
+  displaySettings: boolean;
+
   constructor() { }
+
 
   ngOnInit() {
   }
